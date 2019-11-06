7ef94b939015665a246077baa3d5377882f44c31
Amanbay22 <ertaevamanbai40@gmail.com>
2019-11-06 14:48:51 +0600

    Change base html

---
+++ books/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from .models import Book
+class BookListView(ListView):
+	model = Book
+	template_name = 'books/book_list.html'
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'books/book_detail.html', {'book': book})
